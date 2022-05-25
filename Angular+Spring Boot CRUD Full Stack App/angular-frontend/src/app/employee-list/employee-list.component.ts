4d04eeb8727cc1b43e3c77aec8b1a4fa2ab6b1fa
harshada <harshada.gharge11@gmail.com>
2022-05-26 01:40:31 +0530

    first commit

---
+++ Angular+Spring Boot CRUD Full Stack App/angular-frontend/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service'
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+  employees: Employee[];
+  constructor(private employeeService: EmployeeService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.getEmployees();
+    // this.employees=[{"id":1,"firstName":"Kiran","lastName":"Gharge","emailId":"kiran@gmail.com"}]
+
+
+  
+  }
+
+  private getEmployees(){
+    this.employeeService.getEmployeesList().subscribe(data => {
+      this.employees = data;
+    });
+  }
+
+  employeeDetails(id: number){
+    this.router.navigate(['employee-details', id]);
+  }
+
+  updateEmployee(id: number){
+    this.router.navigate(['update-employee', id]);
+  }
+
+  deleteEmployee(id: number){
+    this.employeeService.deleteEmployee(id).subscribe( data => {
+      console.log(data);
+      this.getEmployees();
+    })
+ }
+
+}
